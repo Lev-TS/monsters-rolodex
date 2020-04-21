39a53ea4ff38998f67bea5417ffedb4bbb53e7db
Lev-TS <57505924+Lev-TS@users.noreply.github.com>
2020-04-22 00:11:15 +0300

    Updates

---
+++ precache-manifest.7b6d8973bce5c9eda25c6b96573908cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3a87ec1ea0ec365512e653e158e3dce",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8e905b20e12c4e315b5a",
+    "url": "/monsters-rolodex/static/css/main.9640a193.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e905b20e12c4e315b5a",
+    "url": "/monsters-rolodex/static/js/main.3e580bdd.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
